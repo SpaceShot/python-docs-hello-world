d5147b944fe11f937507ac179c73ebad10368b07
Chris Gomez <cagomez@cellbycell.com>
2020-11-18 23:54:45 +0000

    added guard clause for repl.it support

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello WPSD Coders"
+
+replit_token = os.getenv("REPLIT")
+
+if replit_token:
+  app.run(host='0.0.0.0', port=8080)
