7088038148bac4db428fe3220e0b4f0903fd242e
Chris Gomez <cagomez@cellbycell.com>
2020-11-19 00:46:18 +0000

    added index.html template

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import os
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello WPSD Coders"
 
+
 @app.route("/page2")
 def page2hello():
     return "Hello WPSD Coders from Page 2"
 
+@app.route("/test")
+def test():
+    return render_template('index.html')
+
 replit_token = os.getenv("REPLIT")
 
-if replit_token:
-  app.run(host='0.0.0.0', port=8080)
+#if replit_token:
+app.run(host='0.0.0.0', port=8080)
